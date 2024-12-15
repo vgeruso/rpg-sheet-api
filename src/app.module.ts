813007064d2b0cb0694ccfa6a0a41a523a2d7ec5
Victor Geruso <victor.geruso@gmail.com>
2024-12-15 10:52:40 -0300

    :bug: fix: non-existent package importation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { SetupModule, SetupService } from '@app/setup';
 
 @Module({
-  imports: [UserModule, SetupModule],
+  imports: [SetupModule],
   controllers: [AppController],
   providers: [AppService, SetupService],
 })
