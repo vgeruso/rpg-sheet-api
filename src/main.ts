043d97fff8903da6147630078783003c302acc51
Victor Geruso <victor.geruso@gmail.com>
2024-12-15 11:47:11 -0300

    :sparkles: feat: install and config of swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SetupService } from '@app/setup';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setDescription(
+      'Documentação voltada ao conhecimento e teste das rotas dos módulos disponíveis no sistema.',
+    )
+    .setTitle('RPG Sheet CORE API')
+    .setVersion((await new SetupService().getSetup()).version)
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
